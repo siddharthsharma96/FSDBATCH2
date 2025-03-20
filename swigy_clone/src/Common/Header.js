a34e58a9882aff1e619b5c70e33a149d452acc7a
codehubnexus <codehubnexus@gmail.com>
2025-03-20 10:30:18 +0530

    Swiggy project creation

--- swigy_clone/src/Common/Header.js
+++ swigy_clone/src/Common/Header.js
@@ -1,14 +1,20 @@
+import NavItems from "../Components/NavItems";
 const Header = () => {
   return (
     <div className="header">
-      <div>
-        <img src={"https://svgsilh.com/svg/2085075.svg"} />
+      <div className="logo__Container">
+        <NavItems path={"/"} type={"img"} src={"images/image.png"} />
       </div>
       <div className="navItems">
         <ul>
-          <li></li>
+          <NavItems path={"/"} name={"Home"} type={"item"} />
+          <NavItems path={"/help"} name={"Help"} type={"item"} />
+          <NavItems path={"/search"} name={"Search"} type={"item"} />
+          <NavItems path={"/cart"} name={"Cart"} type={"item"} />
         </ul>
       </div>
     </div>
   );
 };
+
+export default Header;
